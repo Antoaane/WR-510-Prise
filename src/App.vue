84a688f5d702bb1c3a42dd10718f755a5a4c9c01
Antoine Lauzis <antoine.lauzis@etudiant.univ-reims.fr>
2023-10-20 10:07:32 +0200

    feat: api connect

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,24 @@
 <script setup>
+import { onMounted, ref, watch } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+
+const data = ref()
+
+onMounted(async () => {
+  const response = await axios.get('http://192.168.1.100/status')
+  data.value = response.data
+})
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div v-if="data" class="container">
+    <p>Température : {{ data.temperature }}°C <span v-if="data.overtemperature === true" class="red"></span><span v-else class="green"></span></p>
+    <p>Humidité : {{ data.humidity }}%</p>
+  </div>
+  <p v-else>Chargement des données</p>
+  <pre>
+    {{ data }}
+  </pre>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
